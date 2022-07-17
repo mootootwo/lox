8f23683be193c040250ee1b428334899fbde1852
Luke <mootoo.two@gmail.com>
2022-07-17 18:06:30 +0100

    NPCs being generated at docking ports

--- part-5/js/game.js
+++ part-5/js/game.js
@@ -9,9 +9,16 @@ even though JS isnt supposed to need one of those..
 I couldn't figure out how to do it otherwise.
 */
 
+// randomly generates a non-neutral colour, with "large" difference between RGB
+function paint(){
+    let r = randomRange(0,255);
+    let g = randomRange(0,255);
+    let b = randomRange(0,255);
 
-
-
+    if (Math.abs(((r+g)/2)-b)<50 || Math.abs(((g+b)/2)-r)<50){   // this is a really shit standard deviation calc
+         return paint()
+    }else return "rgba("+r+","+g+","+b+","+1+")";  // maybe I should make concatRGBA() global to use here
+}
 
 
 // should this be a self-initiating function?
@@ -27,12 +34,30 @@ function game(){
     //gameMap = new GameMap(xTiles, yTiles);
     gameMap = generateStation(xTiles,yTiles);
 
-    // engine wont work if these are locally scoped
+    entities = [];
+    npc = [];
+
     let playerStart = gameMap.getRandomClearTile();
     player = new Actor(playerStart.x, playerStart.y, "@", 255,255,255,1,/*"#ffffff",*/ false);
     player.updateFOV();
-    npc = new Actor(player.x+5, player.y+5, "&", 255,0,255,1,/*"#ff00ff",*/ false);
-    entities = [player, npc];
+    entities.push(player);
+
+    for (let i=0;i<dockList.length;i++){
+        let mod=dockList[i].module;
+        let port=dockList[i].port;
+        npc[i] = new Actor(
+            modules[mod].ports[port].x + modules[mod].ports[port].dx,
+            modules[mod].ports[port].y + modules[mod].ports[port].dy,
+            "\u03a6",
+            0,0,0,0,
+            false
+        );
+        npc[i].color = paint();
+        entities.push(npc[i]);
+    };
+       //npc = new Actor(player.x+5, player.y+5, "&", 255,0,255,1,/*"#ff00ff",*/ false);
+    
+    //entities = [player, npc];
 
     engine = new Engine(entities);
 
